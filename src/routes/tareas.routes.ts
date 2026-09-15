10e1967b3e0e5d76759e140353baac296b640cf0
disCarlosBustos <dis.carlosbustos@inba.edu.mx>
2026-09-15 16:15:01 +0000

    feat: prueba unitaria y entorno jest resuelto

---
+++ src/routes/tareas.routes.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { obtenerTareas } from '../controllers/tareas.controller';
+
+const router = express.Router();
+router.get('/tareas', obtenerTareas);
+
+export default router;
